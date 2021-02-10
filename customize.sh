c89efb7c302e7fc09c6a163f7d5930fc65db0589
lunseil <lunseil@qq.com>
2021-02-10 20:07:36 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -7,4 +7,4 @@
 #=================================================
 #1. Modify default IP
 sed -i 's/192.168.1.1/10.10.10.1/g' openwrt/package/base-files/files/bin/config_generate
-sed -i 's/OpenWrt/Soft_Routing/g' openwrt/package/lean/default-settings/files/zzz-default-settings
+#sed -i 's/OpenWrt/Soft_Routing/g' openwrt/package/lean/default-settings/files/zzz-default-settings
