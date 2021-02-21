22c161afabb5d72fd618fa3cea031f2695571d64
lunseil <lunseil@qq.com>
2021-02-21 20:21:03 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -5,5 +5,17 @@
 # Author: eSirPlayground
 # Youtube Channel: https://goo.gl/fvkdwm 
 #=================================================
-#1. Modify default IP
+#1. 修改默认的IP
 sed -i 's/192.168.1.1/10.10.10.1/g' openwrt/package/base-files/files/bin/config_generate
+
+#2. 修改主机名
+sed -i '/uci commit system/i\uci set system.@system[0].hostname='Soft_Routes'' package/lean/default-settings/files/zzz-default-settings
+
+#3. 设置密码为空
+sed -i 's@.*CYXluq4wUazHjmCDBCqXF*@#&@g' package/lean/default-settings/files/zzz-default-settings
+
+#4. 版本号里显示一个自己的名字
+sed -i "s/OpenWrt /Lunseil build $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" package/lean/default-settings/files/zzz-default-settings
+
+#5. # 修改插件名字
+sed -i 's/"Turbo ACC 网络加速"/"网络加速"/g' package/lean/luci-app-sfe/po/zh-cn/sfe.po
